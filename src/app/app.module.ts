6889892dea053d80bb1a0c22824ded4f5685eef6
Hari Krishna K <hari.krishna@walmart.com>
2017-12-23 14:01:08 +0530

    Adding body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { CpuComponent } from './components/body/cpu/cpu.component';
+import { RamComponent } from './components/body/ram/ram.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    CpuComponent,
+    RamComponent
   ],
   imports: [
     BrowserModule
